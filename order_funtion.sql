c9fd4e36170a7f306bc16e40eb960a418ff6de6e
Wahid <islamoahidul12@gmail.com>
2023-11-07 11:28:17 +0600

    Create order_funtion.sql

---
+++ order_funtion.sql
@@ -0,0 +1,9 @@
+select* from Monthly_Salary_SHeet;
+--assending order
+select *
+from Monthly_Salary_SHeet
+order by employee_salary;
+--decending order
+select *
+from Monthly_Salary_SHeet
+order by employee_salary DESC;
