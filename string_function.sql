bc692b972ce0b0d909abbc93ec05a8a69fc97c79
Wahid <islamoahidul12@gmail.com>
2023-11-07 11:03:48 +0600

    Create string_function.sql

---
+++ string_function.sql
@@ -0,0 +1,9 @@
+select*from Monthly_Salary_SHeet;
+
+select lower(employee_name) from Monthly_Salary_SHeet;
+
+select upper(employee_name) from Monthly_Salary_SHeet;
+
+select lower(employee_name) from Monthly_Salary_SHeet where employee_age = 23;
+
+select SUBSTRING(employee_gender,1,1) from Monthly_Salary_SHeet;
